49d858f5c9972c79713ebe11d8872312675fe91c
Michael Kickham <mfkickham3@gmail.com>
2023-11-06 22:36:28 -0600

    Nearly finished the project

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # WeatherTracker
+
+## Description
+I am not quite done with this yet, but the search bar functions and searches for a city. Right now the bootstrap cards overwrite each other, but they still pull the relevant data. I am currently working on a way to make all the cards show independently. The code functions, but it is not pretty. I am hoping to further style it to make it look nicer.
+
+## Sources
+I pulled inspiration from WUSTL 
+The weather API came from the OpenWeatherMap website: https://openweathermap.org/api
+
+## Screenshot
+![image](https://i.gyazo.com/4faad033eef7fc82f9e5a99d6423233d.png)
--- assets/script.js
+++ assets/script.js
@@ -1,39 +1,68 @@
 const apiKey = '66cadb79792bf6bc3526551395b6909c';
 const searchButton = document.querySelector('.search-btn');
 const searchBar = document.querySelector('.search-input');
-const weatherData = document.querySelector('.weather-data');
+const weatherData = document.querySelector('.city-container');
 
 function saveLocalStorage(city, data) {
-    const key = 'weather_data_${city}';
+    const key = `weather_data_${city}`;
     localStorage.setItem(key, JSON.stringify(data));
 }
 
 function getLocalStorage(city){
-    const key = 'weather_data_${city}';
+    const key = `weather_data_${city}`;
     const data = localStorage.getItem(key);
     return data ? JSON.parse(data): null;
 }
 
-searchBar.addEventListener('click', () => {
-    const city = searchInput.value;
-    const apiURL = 'https://api.weatherapi.com/v1/current.json?key=${66cadb79792bf6bc3526551395b6909c}';
-
+searchButton.addEventListener('click', () => {
+    const city = searchBar.value;
+    const apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
+    fiveDayForcast(city);
     fetch(apiURL)
         .then(Response => Response.json())
         .then(data => {
             console.log(data)
-            weatherData.textContent = 'the weather in ${city} is ${data.current.temp_F}°F and ${data.current.condition.text}.';
+            weatherData.innerHTML = `
+            <div class="card" style="width: 18rem;">
+            <div class="card-body">
+              <h5 class="card-title">${city}</h5>
+              <p class="card-text">Temperature: ${data.main.temp}°F</p>
+              <p class="card-text">humidity: ${data.main.humidity}</p>
+              <p class="card-text">Wind Speed: ${data.wind.speed} MPH </p>
+            </div>
+            </div>`;
         })
         .catch(error => {
             console.error('Error fetching the weather data', error)
-            weatherData.textContent = "The weather data you requested is not available, please check later";
+            weatherData.innerHTML = "The weather data you requested is not available, please check later";
         });
 });
 
+function fiveDayForcast(city){
+    const apiTwo = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=imperial`;
+
+    fetch(apiTwo)
+    .then(Response => Response.json())
+    .then(data =>{
+        console.log(data)
+        console.log(data.list.length)
+        for (var i = 0; i < data.list.length; i++) {
+        weatherData.innerHTML = `
+        <div class="card" style="width: 18rem;">
+        <div class="card-body">
+        <h5 class="card-title">${city}</h5>
+        <p class="card-text">Temperature: ${data.list[i].main.temp}°F</p>
+        <p class="card-text">humidity: ${data.list[i].main.humidity}</p>
+        <p class="card-text">Wind Speed: ${data.list[i].wind.speed} MPH </p>
+      </div>`
+    }
+    })
+}
+
 window.addEventListener('load', () => {
-    const city = searchInput.value;
+    const city = searchBar.value;
     const data = getLocalStorage(city);
     if (data) { 
-        weatherData.textContent = 'The weather in ${city} is ${data.current.temp_F}°F and $data.current.condition.text}.';
+        weatherData.textContent = `The weather in ${city} is ${data.current.temp}°F and ${data.current.condition.text}.`;
     }
 })
